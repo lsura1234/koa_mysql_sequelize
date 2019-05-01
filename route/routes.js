cee36ccce1f4d72de0c0bcc2cc359fd1e42f3639
Suradech <allee2470@gmail.com>
2019-05-01 20:48:27 +0700

    update and delete

--- route/routes.js
+++ route/routes.js
@@ -14,6 +14,33 @@ router.post('/', async(ctx, next) => {
      let data = await user.findAll();
     ctx.body = data
  });
+ router.post('/delete',async(ctx,next) =>{
+     let {username} = ctx.request.body
+     await user.destroy({where:{username}}).then((rowDelete) => {
+         if(rowDelete === 1){
+             ctx.body = 'Delete success'
+         }
+         else{
+            ctx.body = 'username not fount'
+         }
+        })
+    })
+    router.post('/update',async(ctx,next) => {
+         let {password,username,id} = ctx.request.body
+       await user.update(
+            {password,username},
+            {where:{id}}
+        ).then((rowUpdate) => {
+          
+            if(rowUpdate[0]===0)
+             {
+                console.log('aaaa') 
+                ctx.body = 'fail'
+            }
+            else 
+                ctx.body = 'success'
+        }) 
+    })
 
 
 
